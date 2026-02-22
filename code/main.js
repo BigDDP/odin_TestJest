9df5625bd2cdfd6bab97734bd06282bb765cc315
Ben Freijah <ben@launchkeylab.com>
2026-02-22 11:56:25 +1100

    feat: Added form submit controller

--- code/main.js
+++ code/main.js
@@ -1,3 +1,17 @@
 
 
-const form = document.querySelector("form")
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach(input => {
+    console.log("loop")
+    input.addEventListener("change", () => {
+        form.requestSubmit();
+    });
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+                
+    console.log("form: ", form);
+});
